848000219caeb75b3434a7b89f882f303cb4fb0e
Vivien Barousse <barousse.vivien@gmail.com>
2011-07-13 11:54:07 +0100

    Added code to support configuration file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
+
   protect_from_forgery
+
+  def conf
+    if @conf.nil?
+      cpath = "#{Rails.root}/config/dropvault.yml"
+      conf = YAML.load_file(cpath) rescue {}
+      @conf = conf['common']
+    end
+    @conf
+  end
+
+  def webdav_endpoint
+    conf['webdav_endpoint']
+  end
+
 end
