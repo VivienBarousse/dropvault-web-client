2fb9d717977723cd12b92c3549bacc9ca430f7f4
Vivien Barousse <barousse.vivien@gmail.com>
2011-07-17 21:11:45 +0100

    Conflicting routes names

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 DropvaultWebClient::Application.routes.draw do
 
-  get "search/search"
+  get "search/search", :as => :search
 
   root :to => 'login#init', :as => :login_init
 
   match 'view/' => 'view#get', :defaults => {:path => '.'}, :as => :view
 
-  match 'view/:path' => 'view#get', :constraints => {:path => /.*/}, :as => :view
+  match 'view/:path' => 'view#get', :constraints => {:path => /.*/}, :as => :viewpath
 
   match 'login/login' => 'login#login', :via => :post, :as => :login
 
