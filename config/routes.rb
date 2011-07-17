cb2e5cc31408abe2c166e878d5950971f3c13934
Vivien Barousse <barousse.vivien@gmail.com>
2011-07-17 21:04:46 +0100

    New name for route view with a :path

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 DropvaultWebClient::Application.routes.draw do
 
+  get "search/search"
+
   root :to => 'login#init', :as => :login_init
 
   match 'view/' => 'view#get', :defaults => {:path => '.'}, :as => :view
 
-  match 'view/:path' => 'view#get', :constraints => {:path => /.*/}
+  match 'view/:path' => 'view#get', :constraints => {:path => /.*/}, :as => :view
 
   match 'login/login' => 'login#login', :via => :post, :as => :login
 
