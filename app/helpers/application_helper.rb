68b935ece03e772479b3788f84ba9c502cd79fb7
Vivien Barousse <barousse.vivien@gmail.com>
2011-09-30 16:29:55 +0200

    Human readable size units

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+  def to_human_size(size)
+    size_units = ["Kb", "Mb", "Gb", "Tb"]
+
+    final_unit = "b"
+    size_units.each do |unit|
+      if size > 1024
+        final_unit = unit
+        size /= 1024
+      end
+    end
+    size.to_s + " " + final_unit
+  end
+
 end
